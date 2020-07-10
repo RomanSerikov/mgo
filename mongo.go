944c5dad6103776a819fc51bbaab26af2b2a784f
Roman Serikov <roman.serikov@yandex.ru>
2020-07-10 17:30:08 +0300

    Added: stopAfterFail option for BulkWrite method

--- mongo.go
+++ mongo.go
@@ -148,10 +148,12 @@ func (db *DB) ReplaceAll(collection string, data []interface{}) error {
 }
 
 // BulkWrite - bulk writes items
-func (db *DB) BulkWrite(collection string, data []mongo.WriteModel) error {
+func (db *DB) BulkWrite(collection string, data []mongo.WriteModel, stopAfterFail bool) error {
 	ctx := context.Background()
+	opts := options.BulkWrite()
+	opts.SetOrdered(stopAfterFail)
 	c := db.Database(db.name).Collection(collection)
-	_, err := c.BulkWrite(ctx, data)
+	_, err := c.BulkWrite(ctx, data, opts)
 	return err
 }
 
